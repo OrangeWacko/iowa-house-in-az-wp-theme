c1c518f94e5a7bbc4dd9f8aedd4cbe7d2372ed54
wizzywiggs <roman@interactiveshift.com>
2019-04-09 10:00:58 -0500

    GA code added

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -11,4 +11,11 @@
   <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAml-SZoTGxLG7Bz79rfrppjR0rSaLq1TI&callback=initMap"></script>
   <link href="https://fonts.googleapis.com/css?family=Pridi|Rubik:400,400i,500,700,700i" rel="stylesheet">
   @php wp_head() @endphp
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-55XQN26');</script>
+  <!-- End Google Tag Manager -->
 </head>
